76f02e2a3389887329aa02088b9592aa084ce7b2
Xabin Rodriguez <xabin95@gmail.com>
2018-05-22 16:27:23 +0200

    its sunny outside lemme go

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { createStore } from 'redux';
+import reducers from './reducers';
+
 
 class App extends Component {
+
+  componentWillMount() {
+    const config = {
+      // get rekt
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>Hello</Text>
         </View>
